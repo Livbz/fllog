01868cc362932777417ce865d9e0b9ba0a751e13
livbz <1025714110@qq.com>
2021-03-29 17:12:42 +0800

    增加搜索

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # fllog
 A blog project based on flask, which is created to be a python-friendly and progressive blog framework.
+
+# build log
+2021.03.17
+- Build a git repository for the project.
+- Realized features
+  - Read the blogs 
+  - Render markdown online instantly
+  - Write blogs on the web and save to the server
--- hello.py
+++ hello.py
@@ -1,18 +1,18 @@
 
 import json
-
+import glob
 from flask import g
 import sqlite3 as sql
 from flask import Flask, render_template, request, session, redirect, url_for, escape
 app = Flask(__name__)
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
 @app.route('/')
 def index():
-    if 'username' in session:
-        return 'Logged in as %s' % escape(session['username'])
-    return 'You are not logged in'
+    # if 'username' in session:
+    #     return 'Logged in as %s' % escape(session['username'])
+    # return 'You are not logged in'
+    return render_template('index.html')
 
 
 @app.route('/hello/<name>')
@@ -40,24 +40,40 @@ def logout():
     return redirect(url_for('index'))
 
 
-@app.route('/blogpage')
+@app.route('/blogpage', methods=['GET','POST'])
 def blogpage(user='visitor'):
-    # con = sql.connect("testDB.db")
-    # con.row_factory = sql.Row
-    # cur = con.cursor()
-    # cur.execute("select * from COMPANY")
-    # raw = cur.fetchall()
-    # blog_text = raw[0]['BTEXT']
-    # con.close()
-    # blog page
+    filepathList = glob.glob('/home/admin/fllog/blogs/*.md')
+    bloglist = dict()
+    for filepath in filepathList:
+        ifPublic = filepath.split('--')[0] # 访问权限
+        title = filepath.split('--')[1]
+        tags = filepath.split('--')[2].split('tag')
+        createdAt = filepath.split('--')[3]
+        editedAt = filepath.split('--')[4]
+        bloglist[title] = {
+            'filepath':filepath,
+            'ifPublic':ifPublic,
+            'tags':tags,
+            'createdAt':createdAt,
+            'editedAt':editedAt,
+        }
+    print(type(bloglist))
+        # bloglist[title] = [filename, ifPublic, tags, createdAt, editedAt]
     # 默认为游客登录
     # md_string = r"## MaHua有哪些功能？\n* 方便的`导入导出`功能\n*  直接把一个markdown的文本文件拖放到当前这个页面就可以了\n*  导出为一个html格式的文件，样式一点也不会丢失\n* 编辑和预览`同步滚动`，所见即所得（右上角设置）\n* `VIM快捷键`支持，方便vim党们快速的操作 （右上角设置）\n* 强大的`自定义CSS`功能，方便定制自己的展示\n* 有数量也有质量的`主题`,编辑器和预览区域\n* 完美兼容`Github`的markdown语法\n* 预览区域`代码高亮`\n* 所有选项自动记忆"
-    with open('/home/admin/workspace/blogs/test3.md', 'r') as fp:
-        r = fp.readlines()
-    a = ''
-    a = a.join(r)
-    print(a)
-    return render_template('blogpage.html', blog_text=a, blog_titile='博客标题')
+    get_Args = request.args
+    args_title = get_Args.get('title')
+    blogArgs_Dict = bloglist.get(args_title)
+    print(type(blogArgs_Dict))
+    blogContent = 'blank'
+    if isinstance(blogArgs_Dict, dict):
+        blogPath = blogArgs_Dict.get('filepath')
+        with open(blogPath, 'r') as fp:
+            allRaw = fp.readlines()
+            a = ''
+            blogContent = a.join(allRaw)
+    print(blogContent)
+    return render_template('blogpage.html', blogTitle='博客标题', blogCreatedAt='', blogEditedAt='', blogContent=blogContent)
 
 
 @app.route('/writeblog')
@@ -72,9 +88,17 @@ def writeblog(user='visitor'):
 def getinput(user='visitor'):
     # request_data = request.get_data()
     getjson = request.get_json()
-    # print(request_data)
+    # b_ for blog_
+    b_title = getjson.get('title')
+    b_tags = getjson.get('tags')
+    b_created_at = getjson.get('created_at')
+    b_edited_at = getjson.get('edited_at')
+    b_content = getjson.get('blogContent')
+    b_ifPublic = '1'
+    blogname_list  = '--'.join([b_ifPublic, b_title, b_tags, b_created_at, b_edited_at])
+
     print(getjson)
-    with open('/home/admin/workspace/blogs/test3.md', 'w') as fp:
+    with open(f'/home/admin/fllog/blogs/{blogname_list}.md', 'w') as fp:
         fp.write(getjson.get('blogContent'))
         # con = sql.connect("DB2.db")
         # con.row_factory = sql.Row
@@ -84,4 +108,24 @@ def getinput(user='visitor'):
         # cur.execute(
         #     f"INSERT INTO `blogs` VALUES('test_two','{blog_Content}');")
         # con.close()
+    return 'ok'
+
+@app.route('/search', methods=['GET'])
+def search(user='visitor'):
+    pattern = '/home/admin/fllog/blogs/*.md'
+    filepath_list =  glob.glob(pattern)
+    bloglist = dict()
+    for filepath in filepath_list:
+            ifPublic = filepath.split('--')[0] # 访问权限
+            title = filepath.split('--')[1]
+            tags = filepath.split('--')[2].split('tag')
+            createdAt = filepath.split('--')[3]
+            editedAt = filepath.split('--')[4]
+            bloglist[title] = {
+                'filepath':filepath,
+                'ifPublic':ifPublic,
+                'tags':tags,
+                'createdAt':createdAt,
+                'editedAt':editedAt,
+            }
     return 'ok'
